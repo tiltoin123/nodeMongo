059a9e1dbfef507c0b9cb8e650584debb5b7610b
italo <italohttp25@gmail.com>
2024-09-09 12:45:14 -0300

    csvsplit ok

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,27 @@
 import { connect, disconect } from './services/mongoClient';
 import { csvLoader } from './services/csvMongo/csvLoader';
 import path from "path";
+import { splitCsv } from './services/csvMongo/splitCsv';
 async function main() {
-    const mongo = await connect();
+    // const mongo = await connect();
 
 
-    const dbName = 'batata2';
-    const collectionName = 'jacare_com_pimenta';
+    // const dbName = 'batata2';
+    // const collectionName = 'jacare_com_pimenta';
 
-    // Use o módulo path para garantir o caminho correto
-    const pathToFile = path.resolve('C:/Users/ISouza/Desktop/empresas/emp/empresas.csv');
+    // // Use o módulo path para garantir o caminho correto
+    // const pathToFile = path.resolve('C:/Users/ISouza/Desktop/empresas/emp/empresas.csv.EMPRECSV');
 
-    // Chama a função csvLoader com os parâmetros corretos
-    const testeLoader = await csvLoader(mongo, pathToFile, collectionName, dbName);
+    // // Chama a função csvLoader com os parâmetros corretos
+    // const testeLoader = await csvLoader(mongo, pathToFile, collectionName, dbName);
 
-    console.log('Resultado do carregamento do CSV:', testeLoader);
+    // console.log('Resultado do carregamento do CSV:', testeLoader);
 
-    // Desconectar do MongoDB
-    await mongo.close();
-    console.log('Desconectado do MongoDB.');
+    // // Desconectar do MongoDB
+    // await mongo.close();
+    // console.log('Desconectado do MongoDB.');
+    let teste = await splitCsv('C:/Users/ISouza/Desktop/empresas/emp/empresas.csv.EMPRECSV', 'C:/Users/ISouza/Desktop/empresas/emp/split_files', 300000);
+    console.log(teste)
 }
 
 // Executa a função principal
---
+++ src/services/csvMongo/splitCsv.ts
@@ -0,0 +1,68 @@
+import fs from 'fs';
+import csv from 'csv-parser';
+
+export const splitCsv = async (inputPath: string, outputDir: string, linesPerFile: number): Promise<void> => {
+    const empHeaders = ["cnpj", "razaoSocial", "naturezaJur", "qualiResponsavel",
+        "capitalSocial", "porteEmpresa", "enteFederativoResponsavel"]
+
+    // const estabHeaders = ["cnpj", "ordem", "digito", "idMatrizFilial",
+    //     "situacao", "dataSituacao", "motivoSituacao", "cidadeExterior",
+    //     "pais", "dataInicio", "cnaePrincipal", "cnaeSecundario", "tipoLogradouro",
+    //     "logradouro", "numero", "complemento", "bairro", "cep", "uf", "municipio",
+    //     "ddd1", "telefone1", "ddd2", "telefone2", "dddFax", "fax", "email", "situacaoEspecial",
+    //     "dataSituacaoEspecial"]
+
+    if (!fs.existsSync(outputDir)) {
+        fs.mkdirSync(outputDir);
+    }
+
+    let lineCount = 0;
+    let fileCount = 1;
+    let rows: Record<string, string>[] = [];
+
+    fs.createReadStream(inputPath)
+        .pipe(csv({
+            // separator: ';',
+            quote: '`',
+            skipLines: 0,
+            headers: empHeaders,
+            // headers: empHeaders
+        }))
+        .on('data', (row: Record<string, string>) => {
+            lineCount++;
+            rows.push(row);
+
+            if (lineCount >= linesPerFile) {
+                // const outputPath = `${outputDir}/estabelecimentos0${fileCount}.csv`;
+                const outputPath = `${outputDir}/empresas.csv.EMPRECSV${fileCount}.csv`;
+                writeCsv(outputPath, rows, empHeaders);
+                console.log("escreveu", `${outputDir}/empresas.csv.EMPRECSV${fileCount}.csv`)
+                fileCount++;
+                lineCount = 0;
+                rows = [];
+            }
+        })
+        .on('end', () => {
+            if (rows.length > 0) {
+                const outputPath = `${outputDir}/empresas.csv.EMPRECSV${fileCount}.csv`;
+                // const outputPath = `${outputDir}/estabelecimentos0${fileCount}.csv`;
+                writeCsv(outputPath, rows, empHeaders);
+                console.log("escreveu", `${outputDir}/empresas.csv.EMPRECSV${fileCount}.csv`)
+                // console.log("escreveu", `${outputDir}/estabelecimentos0${fileCount}.csv`)
+            }
+            console.log('Arquivo CSV dividido com sucesso!');
+        });
+};
+
+const writeCsv = (path: string, data: Record<string, string>[], headers: string[]) => {
+    const quotedHeaders = headers.map(header => `"${header}"`).join(';');
+
+    const rows = data.map(row => Object.values(row).join(';')).join('\n');
+
+    const csvContent = `${quotedHeaders}\n${rows}`;
+
+    fs.writeFileSync(path, csvContent);
+};
+
+// splitCsv('C:/Users/ISouza/Desktop/empresas/emp/empresas.csv.EMPRECSV', 'C:/Users/ISouza/Desktop/empresas/emp/split_files', 100000);
+// splitCsv('C:/Users/ISouza/Desktop/empresas/Estabelecimentos/estabelecimentos0.csv', 'C:/Users/ISouza/Desktop/empresas/Estabelecimentos/split_files', 100000);
